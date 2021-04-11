a852f86c481230558cb47a8592a65b97af235dc2
halhal <hh.25799623@gmail.com>
2021-04-11 10:25:40 +0900

    add test styled component

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import {render} from 'react-dom';
+import styled from 'styled-components';
 
-const Main = (<h1>これはマークダウンエディタでーす...</h1>)
+const Header = styled.h1`
+  color: tomato;
+`;
+
+const Main = (<Header>これはトマト色です</Header>)
 
 render(Main, document.getElementById('app'));
